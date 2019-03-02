d5680e1f8d5559011b686f2ddd139103f7c7243b
Yusuf Hayırlı <47071852+pensarnada@users.noreply.github.com>
2019-03-02 23:51:48 +0300

    some additions

--- Generics.java
+++ Generics.java
@@ -1,6 +1,8 @@
 package javalearning;
 
-	class Generics<T extends Number> {
+import java.util.ArrayList;
+
+class Generics<T extends Number> {
 		//The "<T>" belongs to Number class and it means,
 		//it has types like Integer, Float, Double etc.
 		
@@ -20,14 +22,26 @@ package javalearning;
 			//This shows our value's type as output.
 			
 		}
+		
+		public void GenericT(ArrayList<? extends T> obj) {
+			//"? extends T" means ? must be the subclass of T
+			//Like "Integer" is subclass of "Number"
+			//as we defined "T extends Number" at the Generics class.
+			
+		}
+		
 	public class GenericMain{ //Testing our methods in this class.
 			
 		public void main(String[] args) {
 			
-			Generics<Integer> obj = new Generics<>();
-			//Defined an obj as Integer type by calling "Generics" class and it's Constructor.
+			Generics<Number> obj = new Generics<>();
+			//Defined an obj as Number(includes all types of numbers) type,
+			//by calling "Generics" class and it's Constructor.
 			obj.value=5;
 			obj.Show();//Prints "obj" as value like we defined at "Show" method.
+			obj.GenericT(new ArrayList<Integer>());
+			//What ever you want to give a type this new ArrayList's type,
+			//must be a subclass of T according to GenericT class.
 		}
 	}
 }
